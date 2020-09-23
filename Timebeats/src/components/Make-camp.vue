f55340c7df5b3a3f0edfc064ae61eb038849f83f
XuanSon99 <xuansonn12499@gmail.com>
2020-09-23 13:53:17 +0700

    change

--- Timebeats/src/components/Make-camp.vue
+++ Timebeats/src/components/Make-camp.vue
@@ -460,13 +460,6 @@
                               </tbody>
                             </table>
                           </div>
-                          <paginate
-                            :page-count="20"
-                            :click-handler="functionName"
-                            :prev-text="'Prev'"
-                            :next-text="'Next'"
-                            :container-class="'className'"
-                          ></paginate>
                         </div>
                       </div>
                     </div>
